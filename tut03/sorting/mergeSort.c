0a049ff78309afe960d927f2352ade12a852f7b1
Harry Zhang <z5363493@ad.unsw.edu.au>
2024-06-11 15:12:49 +1000

    code and notes ready for tut03

---
+++ tut03/sorting/mergeSort.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 10
+
+void mergeSort(int A[], int lo, int hi);
+void merge(int A[], int lo, int mid, int hi);
+
+int main(void) {
+
+    // Feel free to play around with this merge sort implementation!
+    int a[SIZE] = {21, 19, 47, -18, -42, -4, 40, 35, 31, -30};
+
+    mergeSort(a, 0, SIZE - 1);
+
+    // The sorted array should be
+    // {-42, -30, -18, -4, 19, 21, 31, 35, 40, 47}
+    for (int i = 0; i < SIZE; i++) {
+        printf("%d ", a[i]);
+    }
+    putchar('\n');
+}
+
+
+
+void mergeSort(int A[], int lo, int hi) {
+	if (lo >= hi) return;
+
+	int mid = (lo + hi) / 2;
+	mergeSort(A, lo, mid);
+	mergeSort(A, mid + 1, hi);
+	merge(A, lo, mid, hi);
+}
+
+void merge(int A[], int lo, int mid, int hi) {
+	int nitems = hi - lo + 1;
+	int *tmp = malloc(nitems * sizeof(int));
+
+	int i = lo;
+	int j = mid + 1;
+	int k = 0;
+
+	// scan both segments into tmp
+	while (i <= mid && j <= hi) {
+		if (A[i] <= A[j]) {
+			tmp[k++] = A[i++];
+		} else {
+			tmp[k++] = A[j++];
+		}
+	}
+
+	// copy items from unfinished segment
+	while (i <= mid) tmp[k++] = A[i++];
+	while (j <= hi)  tmp[k++] = A[j++];
+
+	// copy items from tmp back to main array
+	for (i = lo, k = 0; i <= hi; i++, k++) {
+		A[i] = tmp[k];
+	}
+	free(tmp);
+}
