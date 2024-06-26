9ae873bfde7e2f6c89da91eed9da9695dded016e
Harry Zhang <z5363493@ad.unsw.edu.au>
2024-06-26 10:29:10 +1000

    added tut05 code

--- tut05/balancing/bst.c
+++ tut05/balancing/bst.c
@@ -56,6 +56,7 @@ static struct node *newNode(int val) {
 struct node *rotateRight(struct node *root) {
 	if (root == NULL || root->left == NULL)
 		return root;
+
 	struct node *newRoot = root->left;
 	root->left = newRoot->right;
 	newRoot->right = root;
@@ -64,8 +65,8 @@ struct node *rotateRight(struct node *root) {
 
 struct node *rotateLeft(struct node *root) {
 	if (root == NULL || root->right == NULL)
-
 		return root;
+
 	struct node *newRoot = root->right;
 	root->right = newRoot->left;
 	newRoot->left = root;
